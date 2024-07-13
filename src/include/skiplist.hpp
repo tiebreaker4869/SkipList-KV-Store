063a99579012783ee677d1a883f0af6c5d6f6438
Lifan Sun <lifansun1412@gmail.com>
2024-07-13 14:47:46 +0800

    config clang-format

--- src/include/skiplist.hpp
+++ src/include/skiplist.hpp
@@ -1,250 +1,233 @@
 #pragma once
 
-#include <cstring>
-#include <vector>
-#include <fstream>
 #include <algorithm>
 #include <cstdlib>
+#include <cstring>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
-template <typename Key, typename Value>
-struct Node {
+template <typename Key, typename Value> struct Node {
 
-    Node() = default;
+  Node() = default;
 
-    Node(const Key& k, const Value& v, int l);
+  Node(const Key &k, const Value &v, int l);
 
-    ~Node();
+  ~Node();
 
-    int level;
-    std::vector<Node<Key, Value>*> forward;
-    Key key;
-    Value value;
+  int level;
+  std::vector<Node<Key, Value> *> forward;
+  Key key;
+  Value value;
 };
 
+template <typename Key, typename Value>
+Node<Key, Value>::Node(const Key &k, const Value &v, int l)
+    : key{k}, value{v}, level{l}, forward(l + 1, nullptr) {}
 
-template<typename Key, typename Value>
-Node<Key, Value>::Node(const Key& k, const Value& v, int l): 
-key{k}, value{v}, level{l}, forward(l + 1, nullptr) 
-{
-    
-}
+template <typename Key, typename Value> Node<Key, Value>::~Node() {}
 
-template<typename Key, typename Value>
-Node<Key, Value>::~Node() {
-    
-}
-
-
-template<typename Key, typename Value>
-class SkipList {
+template <typename Key, typename Value> class SkipList {
 public:
-    SkipList(const SkipList&) = delete;
+  SkipList(const SkipList &) = delete;
 
-    SkipList& operator=(const SkipList&) = delete;
+  SkipList &operator=(const SkipList &) = delete;
 
-    SkipList();
-    
-    ~SkipList();
-    
-    int RandomHeight() const;
+  SkipList();
 
-    bool Insert(const Key& key, const Value& value);
+  ~SkipList();
 
-    bool Contains(const Key& key) const;
+  int RandomHeight() const;
 
-    const Value* Get(const Key& key) const;
+  bool Insert(const Key &key, const Value &value);
 
-    void Delete(const Key& key);
+  bool Contains(const Key &key) const;
 
-    void Display() const;
+  const Value *Get(const Key &key) const;
 
-    int Size() const;
+  void Delete(const Key &key);
 
-    void LoadFromFile(const std::string& filename);
+  void Display() const;
 
-    void DumpToFile(const std::string& filename);
+  int Size() const;
 
+  void LoadFromFile(const std::string &filename);
+
+  void DumpToFile(const std::string &filename);
 
 private:
-    enum {kMaxLevel = 13};// 最大 level
-    int current_level_; // skiplist 当前的 level
-    Node<Key, Value> head_; // 虚拟的头节点
-    int num_element_; // 包含的实际节点的个数
+  enum { kMaxLevel = 13 }; // 最大 level
+  int current_level_;      // skiplist 当前的 level
+  Node<Key, Value> head_;  // 虚拟的头节点
+  int num_element_;        // 包含的实际节点的个数
 
-    Node<Key, Value>* NewNode(const Key& key, const Value& value, const int level);
+  Node<Key, Value> *NewNode(const Key &key, const Value &value,
+                            const int level);
 
-    void DeleteRecursively(const Key& key);
+  void DeleteRecursively(const Key &key);
 };
 
 template <typename Key, typename Value>
-SkipList<Key, Value>::SkipList(): current_level_{0}, head_{Key{}, Value{}, kMaxLevel},
-num_element_{0} {
+SkipList<Key, Value>::SkipList()
+    : current_level_{0}, head_{Key{}, Value{}, kMaxLevel}, num_element_{0} {}
 
-}
-
-
-template <typename Key, typename Value>
-SkipList<Key, Value>::~SkipList() {
-    auto current = head_.forward[0];
-    while (current) {
-        auto next = current->forward[0];
-        delete current;
-        current = next;
-    }
+template <typename Key, typename Value> SkipList<Key, Value>::~SkipList() {
+  auto current = head_.forward[0];
+  while (current) {
+    auto next = current->forward[0];
+    delete current;
+    current = next;
+  }
 }
 
 template <typename Key, typename Value>
-Node<Key, Value>* SkipList<Key, Value>::NewNode(const Key& key, const Value& value, const int level) {
-    auto new_node = new Node<Key, Value>(key, value, level);
-    return new_node;
+Node<Key, Value> *SkipList<Key, Value>::NewNode(const Key &key,
+                                                const Value &value,
+                                                const int level) {
+  auto new_node = new Node<Key, Value>(key, value, level);
+  return new_node;
 }
 
 template <typename Key, typename Value>
 int SkipList<Key, Value>::RandomHeight() const {
-    int level = 1;
+  int level = 1;
 
-    // 模拟抛硬币决定是否上升层级
-    while (rand() % 2 && level < kMaxLevel) {
-        level ++;
-    }
+  // 模拟抛硬币决定是否上升层级
+  while (rand() % 2 && level < kMaxLevel) {
+    level++;
+  }
 
-    return level;
+  return level;
 }
 
 template <typename Key, typename Value>
-const Value* SkipList<Key, Value>::Get(const Key& key) const {
-    auto current = &head_;
+const Value *SkipList<Key, Value>::Get(const Key &key) const {
+  auto current = &head_;
 
-    for (int i = current_level_; i >= 0; i --) {
-        // 在每一层尝试往前走, 走到头则下沉
-        while (current->forward[i] && current->forward[i]->key < key) {
-            current = current->forward[i];
-        }
+  for (int i = current_level_; i >= 0; i--) {
+    // 在每一层尝试往前走, 走到头则下沉
+    while (current->forward[i] && current->forward[i]->key < key) {
+      current = current->forward[i];
     }
+  }
 
-    // 看最后一层是否有要找的 key(所有的 key 在最后一层都会出现, 如果没有那就没有)
-    current = current->forward[0];
+  // 看最后一层是否有要找的 key(所有的 key 在最后一层都会出现, 如果没有那就没有)
+  current = current->forward[0];
 
-    if (current && current->key == key) {
-        return &current->value;
-    }
+  if (current && current->key == key) {
+    return &current->value;
+  }
 
-    return nullptr;
+  return nullptr;
 }
 
 template <typename Key, typename Value>
-bool SkipList<Key, Value>::Contains(const Key& key) const {
-    return Get(key) != nullptr;
+bool SkipList<Key, Value>::Contains(const Key &key) const {
+  return Get(key) != nullptr;
 }
 
 template <typename Key, typename Value>
-bool SkipList<Key, Value>::Insert(const Key& key, const Value& value) {
-    auto current = &head_;
+bool SkipList<Key, Value>::Insert(const Key &key, const Value &value) {
+  auto current = &head_;
 
-    std::vector<Node<Key, Value>*> updated(kMaxLevel + 1, nullptr);
+  std::vector<Node<Key, Value> *> updated(kMaxLevel + 1, nullptr);
 
-    for (int i = current_level_; i >= 0; i --) {
-        while (current->forward[i] && current->forward[i]->key < key) {
-            current = current->forward[i];
-        }
-        updated[i] = current;
+  for (int i = current_level_; i >= 0; i--) {
+    while (current->forward[i] && current->forward[i]->key < key) {
+      current = current->forward[i];
+    }
+    updated[i] = current;
+  }
+
+  current = current->forward[0];
+
+  // 待插入的 key 已经存在
+  if (current && current->key == key) {
+    return false;
+  }
+
+  // 插入过程
+  int level = RandomHeight();
+
+  // 如果插入的节点 level 大于当前 level
+  if (level > current_level_) {
+    for (int i = current_level_ + 1; i <= level; i++) {
+      updated[i] = &head_;
     }
 
-    current = current->forward[0];
+    current_level_ = level;
+  }
 
-    // 待插入的 key 已经存在
-    if (current && current->key == key) {
-        return false;
-    }
+  // 创建新节点
+  auto node = NewNode(key, value, level);
 
-    // 插入过程
-    int level = RandomHeight();
+  // 在各层插入新节点, 更新 forward 指针
+  for (int i = 0; i <= level; i++) {
+    node->forward[i] = updated[i]->forward[i];
+    updated[i]->forward[i] = node;
+  }
 
-    // 如果插入的节点 level 大于当前 level
-    if (level > current_level_) {
-        for (int i = current_level_ + 1; i <= level; i ++) {
-            updated[i] = &head_;
-        }
+  num_element_++;
 
-        current_level_ = level;
-    }
+  return true;
+}
 
-    // 创建新节点
-    auto node = NewNode(key, value, level);
-
-    // 在各层插入新节点, 更新 forward 指针
-    for (int i = 0; i <= level; i ++) {
-        node->forward[i] = updated[i]->forward[i];
-        updated[i]->forward[i] = node;
-    }
-
-    num_element_ ++;
-
-    return true;
+template <typename Key, typename Value> int SkipList<Key, Value>::Size() const {
+  return num_element_;
 }
 
 template <typename Key, typename Value>
-int SkipList<Key, Value>::Size() const {
-    return num_element_;
-}
+void SkipList<Key, Value>::Delete(const Key &key) {
+  auto current = &head_;
+  std::vector<Node<Key, Value> *> updated(kMaxLevel + 1, nullptr);
 
-template <typename Key, typename Value>
-void SkipList<Key, Value>::Delete(const Key& key) {
-    auto current = &head_;
-    std::vector<Node<Key, Value>*> updated(kMaxLevel + 1, nullptr);
-
-    for (int i = current_level_; i >= 0; i --) {
-        while (current->forward[i] && current->forward[i]->key < key) {
-            current = current->forward[i];
-        }
-        // 记录在每一层可能的前驱
-        updated[i] = current;
+  for (int i = current_level_; i >= 0; i--) {
+    while (current->forward[i] && current->forward[i]->key < key) {
+      current = current->forward[i];
     }
+    // 记录在每一层可能的前驱
+    updated[i] = current;
+  }
 
-    // 是否找到待删除节点
-    current = current->forward[0];
-    if (current == nullptr || current->key != key) {
-        return;
-    }
+  // 是否找到待删除节点
+  current = current->forward[0];
+  if (current == nullptr || current->key != key) {
+    return;
+  }
 
-    // 只有在 0 - node 的 level 之间 node 有前驱
-    for (int i = 0; i <= current->level; i ++) {
-        updated[i]->forward[i] = current->forward[i];
-    }
+  // 只有在 0 - node 的 level 之间 node 有前驱
+  for (int i = 0; i <= current->level; i++) {
+    updated[i]->forward[i] = current->forward[i];
+  }
 
-    // 调整当前 skiplist 整体 level
-    while (current_level_ > 0 && head_.forward[current_level_] == nullptr) {
-        current_level_ --;
-    }
+  // 调整当前 skiplist 整体 level
+  while (current_level_ > 0 && head_.forward[current_level_] == nullptr) {
+    current_level_--;
+  }
 
-    delete current;
+  delete current;
 
-    num_element_ --;
+  num_element_--;
 }
 
 template <typename Key, typename Value>
 void SkipList<Key, Value>::Display() const {
-    for (int i = current_level_; i >= 0; i --) {
-        std::cout << "Level " << i << ":";
+  for (int i = current_level_; i >= 0; i--) {
+    std::cout << "Level " << i << ":";
 
-        auto current = head_.forward[i];
-        while (current) {
-            std::cout << "(" << current->key << ", " << current->value << ") ";
-            current = current->forward[i];
-        }
-
-        // 本层输出结束
-        std::cout << std::endl;
+    auto current = head_.forward[i];
+    while (current) {
+      std::cout << "(" << current->key << ", " << current->value << ") ";
+      current = current->forward[i];
     }
+
+    // 本层输出结束
+    std::cout << std::endl;
+  }
 }
 
 template <typename Key, typename Value>
-void SkipList<Key, Value>::LoadFromFile(const std::string& filename) {
-
-}
+void SkipList<Key, Value>::LoadFromFile(const std::string &filename) {}
 
 template <typename Key, typename Value>
-void SkipList<Key, Value>::DumpToFile(const std::string& filename) {
-    
-}
+void SkipList<Key, Value>::DumpToFile(const std::string &filename) {}
--- tests/skiplist_test.cpp
+++ tests/skiplist_test.cpp
@@ -1,5 +1,5 @@
-#include <gtest/gtest.h>
 #include <cstdint>
+#include <gtest/gtest.h>
 #include <map>
 
 #include "skiplist.hpp"
@@ -9,87 +9,87 @@ using Key = uint64_t;
 using Value = uint64_t;
 
 TEST(SkipTest, Empty) {
-    SkipList<Key, Value> skip_list{};
-    EXPECT_EQ(skip_list.Get(1), nullptr);
-    EXPECT_EQ(skip_list.Contains(1), false);
+  SkipList<Key, Value> skip_list{};
+  EXPECT_EQ(skip_list.Get(1), nullptr);
+  EXPECT_EQ(skip_list.Contains(1), false);
 }
 
 TEST(SkipTest, InsertSimple) {
-    // simply test insert functionality
-    SkipList<Key, Value> skip_list{};
-    skip_list.Insert(1, 1);
-    EXPECT_EQ(*skip_list.Get(1), 1);
-    EXPECT_EQ(skip_list.Contains(1), true);
+  // simply test insert functionality
+  SkipList<Key, Value> skip_list{};
+  skip_list.Insert(1, 1);
+  EXPECT_EQ(*skip_list.Get(1), 1);
+  EXPECT_EQ(skip_list.Contains(1), true);
 }
 
 TEST(SkipTest, DeleteSimple) {
-    // simple test delete functionality
-    SkipList<Key, Value> skip_list{};
-    skip_list.Delete(1);
-    EXPECT_EQ(skip_list.Get(1), nullptr);
-    skip_list.Insert(1, 4);
-    EXPECT_EQ(skip_list.Contains(1), true);
-    skip_list.Delete(1);
-    EXPECT_EQ(skip_list.Get(1), nullptr);
+  // simple test delete functionality
+  SkipList<Key, Value> skip_list{};
+  skip_list.Delete(1);
+  EXPECT_EQ(skip_list.Get(1), nullptr);
+  skip_list.Insert(1, 4);
+  EXPECT_EQ(skip_list.Contains(1), true);
+  skip_list.Delete(1);
+  EXPECT_EQ(skip_list.Get(1), nullptr);
 }
 
 TEST(SkipTest, SizeTest) {
-    SkipList<Key, Value> skip_list{};
-    for (int i = 0; i < 100; i++) {
-        skip_list.Insert(i, i);
-        EXPECT_EQ(skip_list.Size(), i + 1);
-    }
+  SkipList<Key, Value> skip_list{};
+  for (int i = 0; i < 100; i++) {
+    skip_list.Insert(i, i);
+    EXPECT_EQ(skip_list.Size(), i + 1);
+  }
 
-    for (int i = 0; i < 100; i++) {
-        skip_list.Delete(i);
-        EXPECT_EQ(skip_list.Size(), 100 - i - 1);
-    }
+  for (int i = 0; i < 100; i++) {
+    skip_list.Delete(i);
+    EXPECT_EQ(skip_list.Size(), 100 - i - 1);
+  }
 }
 
 TEST(SkipTest, DiffTestAgainstMap) {
-    constexpr int N = 1000000;
-    std::map<Key, Value> map{};
-    SkipList<Key, Value> skip_list{};
+  constexpr int N = 1000000;
+  std::map<Key, Value> map{};
+  SkipList<Key, Value> skip_list{};
 
-    for (int i = 0; i < N; i ++) {
-        int test_func = rand() % 5; 
-        Key key = rand();
-        Value value = rand();
-        // choose tested operation randomly
-        // 0 - Insert, 1 - Contains, 2 - Get, 3 - Delete, 4 - Size
-        switch(test_func) {
-            case 0:
-                map[key] = value;
-                if (skip_list.Contains(key)) {
-                    skip_list.Delete(key);
-                }
-                skip_list.Insert(key, value);
-                break;
-            case 1:
-                EXPECT_EQ(skip_list.Contains(key), map.count(key));
-                break;
-            case 2:
-                if (map.count(key)) {
-                    EXPECT_EQ(*skip_list.Get(key), map[key]);
-                } else {
-                    EXPECT_EQ(skip_list.Get(key), nullptr);
-                }
-                break;
-            case 3:
-                map.erase(key);
-                skip_list.Delete(key);
-                break;
-            case 4:
-                EXPECT_EQ(skip_list.Size(), map.size());
-                break;
-            default:
-                break;
-        }
+  for (int i = 0; i < N; i++) {
+    int test_func = rand() % 5;
+    Key key = rand();
+    Value value = rand();
+    // choose tested operation randomly
+    // 0 - Insert, 1 - Contains, 2 - Get, 3 - Delete, 4 - Size
+    switch (test_func) {
+    case 0:
+      map[key] = value;
+      if (skip_list.Contains(key)) {
+        skip_list.Delete(key);
+      }
+      skip_list.Insert(key, value);
+      break;
+    case 1:
+      EXPECT_EQ(skip_list.Contains(key), map.count(key));
+      break;
+    case 2:
+      if (map.count(key)) {
+        EXPECT_EQ(*skip_list.Get(key), map[key]);
+      } else {
+        EXPECT_EQ(skip_list.Get(key), nullptr);
+      }
+      break;
+    case 3:
+      map.erase(key);
+      skip_list.Delete(key);
+      break;
+    case 4:
+      EXPECT_EQ(skip_list.Size(), map.size());
+      break;
+    default:
+      break;
     }
+  }
 }
 
 // start run main
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
